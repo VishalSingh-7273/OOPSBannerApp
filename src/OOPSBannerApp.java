cfbbc264790283e2e95962464263724ede41eb25
vishal singh <vishal1singh4444@gmail.com>
2026-01-29 16:55:18 +0530

    uc7 using CharacterPattern class

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -1,48 +1,57 @@
 public class OOPSBannerApp {
 
-    static String[] getO() {
-        return new String[]{
-                " ***** ",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                "*     *",
-                " ***** "
-        };
+    static class CharacterPattern {
+        private final char ch;
+        private final String[] pattern;
+
+        CharacterPattern(char ch, String[] pattern) {
+            this.ch = ch;
+            this.pattern = pattern;
+        }
+
+        String[] getPattern() {
+            return pattern;
+        }
     }
 
-    static String[] getP() {
-        return new String[]{
-                " ***** ",
-                "*     *",
-                "*     *",
-                " ***** ",
-                "*      ",
-                "*      ",
-                "*      "
-        };
-    }
+    static CharacterPattern O = new CharacterPattern('O', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            "*     *",
+            " ***** "
+    });
 
-    static String[] getS() {
-        return new String[]{
-                " ***** ",
-                "*      ",
-                "*      ",
-                " ***** ",
-                "     *",
-                "     *",
-                " ***** "
-        };
-    }
+    static CharacterPattern P = new CharacterPattern('P', new String[]{
+            " ***** ",
+            "*     *",
+            "*     *",
+            " ***** ",
+            "*      ",
+            "*      ",
+            "*      "
+    });
+
+    static CharacterPattern S = new CharacterPattern('S', new String[]{
+            " ***** ",
+            "*      ",
+            "*      ",
+            " ***** ",
+            "     *",
+            "     *",
+            " ***** "
+    });
 
     public static void main(String[] args) {
-        String[] o = getO();
-        String[] p = getP();
-        String[] s = getS();
-
         for (int i = 0; i < 7; i++) {
-            System.out.println(o[i] + o[i] + p[i] + s[i]);
+            System.out.println(
+                    O.getPattern()[i] +
+                            O.getPattern()[i] +
+                            P.getPattern()[i] +
+                            S.getPattern()[i]
+            );
         }
     }
 }
